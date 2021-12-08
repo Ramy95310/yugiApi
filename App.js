707b399f3df3f2fb6d365d080fc030bb41a65c94
Saidane Ramy <saidane.ramy@gmail.fr>
2021-12-08 15:38:51 +0100

    homepage

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import { getValues } from 'jest-validate/build/condition';
 import React from 'react'
-import {SafeAreaView, StyleSheet, Text, View, TextInput, FormDataEvent, Button} from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, TextInput, FormDataEvent, Button } from 'react-native'
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
-    return (
-      <SafeAreaView style={styles.container}>
-       
-        <View style={styles.gris}></View>
-          <Text style={styles.screen}>Inscription</Text>
+function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
 
-          <TextInput placeholder={'Prénom'}  onChangeText={ () => console.log('onChangeText')}
-                       onEndEditing={ () => console.log('Input end')}/>        
-      </SafeAreaView>
-    );
+      <View style={styles.gris}></View>
+      <Text style={styles.screen}>Inscription</Text>
 
-};
+      <TextInput placeholder={'Prénom'} onChangeText={() => console.log('onChangeText')}
+        onEndEditing={() => console.log('Input end')} />
+    </SafeAreaView>
+  );
+}
 
-const styles = StyleSheet.create({
-gris: {
-  flex : 5,
-  backgroundColor: "grey",
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
+
+
+const styles = StyleSheet.create({
+  gris: {
+    flex: 5,
+    backgroundColor: "grey",
+  },
+});
