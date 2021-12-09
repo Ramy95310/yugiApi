e5406adb6f62e123c353360522a74996b272e0d8
houssam <hlaghzil@gmail.com>
2021-12-09 18:23:16 +0100

    debug

--- App.js
+++ App.js
@@ -1,62 +1,92 @@
-import React , { useState } from 'react'
-import { SafeAreaView, StyleSheet, Text, View, TextInput, FormDataEvent, Button } from 'react-native'
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FormDataEvent,
+  Button,
+} from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AsyncStorage from "@react-native-community/async-storage";
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function HomeScreen() {
-  const [myInputname, setmyInputname]=useState("");
-  const [myInputmdp, setmyInputmdp]=useState("");
+  const [myInputname, setmyInputname] = useState('');
 
-    const storeData = async (myInputname, myInputmdp) => {
-        try {
-            const dataname = JSON.stringify(myInputname)
-            const datamdp = JSON.stringify(myInputmdp)
-            AsyncStorage.setItem('@name', dataname)
-            AsyncStorage.setItem('@mdp', datamdp)
-            console.log("store: "+ " " + dataname +" " +datamdp)
-        } catch (e) {
-            // saving error
-            console.log("caca")
-        }
+  const [myInputmdp, setmyInputmdp] = useState('');
+  console.log(myInputmdp);
+  const storeData = async (myInputname, myInputmdp) => {
+    try {
+      const dataname = JSON.stringify(myInputname);
+      const datamdp = JSON.stringify(myInputmdp);
+      AsyncStorage.setItem('@name', dataname);
+      AsyncStorage.setItem('@mdp', datamdp);
+      console.log('store: ' + ' ' + dataname + ' ' + datamdp);
+    } catch (e) {
+      // saving error
+      console.log('caca');
     }
+  };
 
-    const getData = async () => {
-        try {
-            const dataname = await AsyncStorage.getItem('@name')
-            const datamdp = await AsyncStorage.getItem('@mdp')
+  const getData = async () => {
+    try {
+      const dataname = await AsyncStorage.getItem('@name');
+      const datamdp = await AsyncStorage.getItem('@mdp');
 
-            if(dataname != null || datamdp != null) {
-                const dataall = JSON.parse(datamdp) + " " + JSON.parse(dataname)
+      if (dataname != null || datamdp != null) {
+        const dataall = JSON.parse(datamdp) + ' ' + JSON.parse(dataname);
 
-                // value previously stored
-                return console.log(dataall)
-            }
-        } catch(e) {}
-    }
+        // value previously stored
+        return console.log(dataall);
+      }
+    } catch (e) {}
+  };
 
   return (
     <SafeAreaView style={styles.gris}>
-
-      <View style={styles.gris}/>
+      <View style={styles.gris} />
       <Text>Inscription</Text>
 
-      <TextInput backgoundcolor="#0000" color="#841584" placeholder={'pseudo'} value={myInputname}
-          onChangeText={setmyInputname} onEndEditing={() =>console.log(myInputname)}/>
+      <TextInput
+        backgoundcolor="#0000"
+        color="#841584"
+        placeholder={'pseudo'}
+        value={myInputname}
+        onChangeText={setmyInputname}
+        onEndEditing={() => console.log(myInputname)}
+      />
 
-          <TextInput secureTextEntry={true} color="#841584" placeholder={'mdp'} value={myInputmdp}
-          onChangeText={setmyInputmdp} onEndEditing={() =>console.log(myInputmdp)}/> 
+      <TextInput
+        secureTextEntry={true}
+        color="#841584"
+        placeholder={'mdp'}
+        value={myInputmdp}
+        onChangeText={setmyInputmdp}
+        onEndEditing={() => console.log(myInputmdp)}
+      />
 
-          <Button onPress={() =>{const myInput ="name: " + myInputname +" mdp: "+ myInputmdp ; console.log(myInput);return  storeData( myInputname, myInputmdp)}} title="inscription"
-                  color="#841584"
-                  accessibilityLabel="Learn more about this purple button"/>
+      <Button
+        onPress={() => {
+          const myInput = 'name: ' + myInputname + ' mdp: ' + myInputmdp;
+          console.log(myInput);
+          return storeData(myInputname, myInputmdp);
+        }}
+        title="inscription"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
 
-        <Button onPress={() =>{return  getData()}} title="get"
-                  color="#841584"
-                  accessibilityLabel="Learn more about this purple button"/>
-
+      <Button
+        onPress={() => {
+          return getData();
+        }}
+        title="get"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </SafeAreaView>
   );
 }
@@ -75,10 +105,9 @@ function App() {
 
 export default App;
 
-
 const styles = StyleSheet.create({
   gris: {
     flex: 5,
-    backgroundColor: "grey",
+    backgroundColor: 'grey',
   },
 });
